bb0640a121070a78e756d2127fe55afd93c32281
paul <npn.perm@gmail.com>
2020-04-15 17:38:24 +0500

    1

---
+++ NU_L6_3.py
@@ -0,0 +1,45 @@
+# урок 6, задание 3
+
+
+from pprint import pprint
+
+
+def compare(S1, S2):
+    ngrams = [
+        S1[i:i + 3] for i in range(len(S1))
+    ]
+
+    count = 0
+    for ngram in ngrams:
+        count += S2.count(ngram)
+    return count / max(len(S1), len(S2))
+
+
+class Cat:
+    def __init__(self, name, breed, nose, tail, mustache, tum):
+        self.name, self.breed, self.nose, self.tail, self.mustache, self.tum = name, breed, nose, tail, mustache, tum
+        self.key = (name, breed)
+
+    def __repr__(self):
+        return "Cat('%s','%s','%s','%s','%s','%s')" % (
+            self.name, self.breed, self.nose, self.tail, self.mustache, self.tum)
+
+    def __eq__(self, obj):
+        if type(obj) == str:
+            for x in [self.name, self.breed, self.nose, self.tail, self.mustache, self.tum]:
+                if compare(obj, x) >= 0.5:
+                    return True
+
+
+Kashtan = Cat("Kashtan", "abyssinian", "wet", "fluffy", "long", "soft")
+Murzik = Cat("Murzik", "bengal", "dry", "fluffy", "short", "soft")
+Cats = {
+    Kashtan.key: Kashtan,
+    Murzik.key: Murzik,
+}
+
+inputValue = "sry"
+
+for cat in Cats.values():
+    if cat == inputValue:
+        print("Нашелся котейка %s" % cat)
