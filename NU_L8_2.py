f775fa9a35a6d1c8218801cd3364a3b39b497e19
paul <npn.perm@gmail.com>
2020-04-17 16:39:10 +0500

    Урок 8, задание 2 Гадалка в цикле, запоминает связку вопрос-ответ проверяет вопросы на ранее задававшиеся

--- NU_L8_2.py
+++ NU_L8_2.py
@@ -1,6 +1,7 @@
 # урок 8, задание 2
 
 # гадалка в цикле, запоминает связку вопрос-ответ
+# проверяет вопросы на ранее задававшиеся
 
 import random
 
@@ -10,9 +11,12 @@ questions = {}
 while question != "Стоп":
     question = input("Задай свой вопрос: ")
     if question[-1] == "?" and question != "Стоп":
-        a = random.randint(0, 1)
-        print("Да") if a == 1 else print("Нет")
-        questions[question] = a
+        if question in questions.keys():
+            print("Ты уже спрашивал")
+        else:
+            a = random.randint(0, 1)
+            print("Да") if a == 1 else print("Нет")
+            questions[question] = a
     else:
         print("Задай вопрос, я не понимаю")
 
